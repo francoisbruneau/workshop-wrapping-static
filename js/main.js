4d7c83dda74f4b577faa06d2bb05e042cbe9200d
François Bruneau <francois.bruneau@liip.ch>
2014-11-16 15:30:04 +0100

    added thank you message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function GetURLParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
 
+
+jQuery(document).ready(function (){
+	var hasRegistered = GetURLParameter('thank_you');
+	if (hasRegistered == 1) {
+		jQuery("a.btn-primary").html("Merci !");
+	}
+});
